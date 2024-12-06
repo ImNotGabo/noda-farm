b2888e250b5cc1189a75cbd7d609c2f958d88fce
ImNotGabo <gabriel.duarte18@outlook.es>
2024-12-06 08:30:07 -0400

    fillinh the templates

--- index.js
+++ index.js
@@ -42,16 +42,57 @@ console.log('Will read file!'); */
 // SERVER
 
 // Top-lvl code to execute one time
+const templateOverview = fs.readFileSync(
+	`${__dirname}/templates/template-overview.html`,
+	'utf-8'
+);
+const templateCard = fs.readFileSync(
+	`${__dirname}/templates/template-card.html`,
+	'utf-8'
+);
+const templateProduct = fs.readFileSync(
+	`${__dirname}/templates/template-product.html`,
+	'utf-8'
+);
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const productData = JSON.parse(data);
+
+const replaceTemplate = (template, product) => {
+	let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+	output = output.replace(/{%IMAGE%}/g, product.image);
+	output = output.replace(/{%PRICE%}/g, product.price);
+	output = output.replace(/{%FROM%}/g, product.from);
+	output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+	output = output.replace(/{%QUANTITY%}/g, product.quantity);
+	output = output.replace(/{%DESCRIPTION%}/g, product.description);
+	output = output.replace(/{%ID%}/g, product.id);
+
+	if (!product.organic)
+		output = output.replaceAll('{%NOT_ORGANIC%}', 'not-organic');
+	return output;
+};
 
 const server = http.createServer((req, res) => {
 	console.log(req.url);
 	const pathName = req.url;
+
+	// Overview page
 	if (pathName === '/' || pathName === '/overview') {
-		res.end('This is the OVERVIEW');
+		res.writeHead(200, {
+			'Content-type': 'text/html',
+		});
+
+		const cards = productData
+			.map((element) => replaceTemplate(templateCard, element))
+			.join('');
+		const output = templateOverview.replace('{%PRODUCT_CARDS%}', cards);
+		res.end(output);
+
+		// Product page
 	} else if (pathName === '/product') {
 		res.end('This is the PRODUCT');
+
+		// API
 	} else if (pathName === '/api') {
 		fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (_, data) => {
 			res.writeHead(200, {
@@ -59,6 +100,8 @@ const server = http.createServer((req, res) => {
 			});
 			res.end(data);
 		});
+
+		// Not found
 	} else {
 		res.writeHead(404, {
 			'Content-type': 'text/html',
