8318ae1d37d58c904850579cb1d00a155b441df2
ImNotGabo <gabriel.duarte18@outlook.es>
2024-12-07 21:41:03 -0400

    replaceTemplate exported

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const { URL } = require('url');
+const { replaceTemplate } = require('./modules/replaceTemplate');
 // const url = require('url'); // Deprecated
 
 //////////////////////////////////////////
@@ -57,21 +58,6 @@ const templateProduct = fs.readFileSync(
 );
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const productData = JSON.parse(data);
-
-const replaceTemplate = (template, product) => {
-	let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
-	output = output.replace(/{%IMAGE%}/g, product.image);
-	output = output.replace(/{%PRICE%}/g, product.price);
-	output = output.replace(/{%FROM%}/g, product.from);
-	output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-	output = output.replace(/{%QUANTITY%}/g, product.quantity);
-	output = output.replace(/{%DESCRIPTION%}/g, product.description);
-	output = output.replace(/{%ID%}/g, product.id);
-
-	if (!product.organic)
-		output = output.replaceAll('{%NOT_ORGANIC%}', 'not-organic');
-	return output;
-};
 
 const server = http.createServer((req, res) => {
 	const BASE_URL = `http://127.0.0.1:8000`;
